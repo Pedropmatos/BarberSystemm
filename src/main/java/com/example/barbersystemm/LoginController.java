12da0d3d5f8a93c4b2d6f76ac62cd6214c8cf712
Pedro Paulo <Pedropmp.trab@outlook.com>
2024-06-02 21:55:07 -0300

    Segunda tela

---
+++ src/main/java/com/example/barbersystemm/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.barbersystemm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LoginController {
+   @FXML
+    private Button btLogin;
+
+
+    @FXML
+   public void onbtLoginAction(){
+       System.out.println("Click");
+   }
+}
